8713d35702553a8d933b9653dfca44715d1dc6fc
TraceOfHumanity <azalexxx02@gmail.com>
2026-03-20 11:47:32 +0200

    feat: update base shader example with plane geometry and transparency adjustments

--- src/experience/base-shader-example/base-shader-example.tsx
+++ src/experience/base-shader-example/base-shader-example.tsx
@@ -1,19 +1,22 @@
 import vertexShader from "../../shaders/base-shader/vertex.vert?raw";
 import fragmentShader from "../../shaders/base-shader/fragment.frag?raw";
+import { DoubleSide } from "three";
 
 const BaseShaderExample = () => {
   return (
     <>
       <mesh>
-        <boxGeometry />
+        <planeGeometry args={[1, 1, 16, 16]} />
         <rawShaderMaterial
           vertexShader={vertexShader}
           fragmentShader={fragmentShader}
+          side={DoubleSide}
+          transparent={true}
         />
       </mesh>
       <mesh position={[-2, 0, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color="red" />
+        <planeGeometry />
+        <meshStandardMaterial color="red" side={DoubleSide} />
       </mesh>
     </>
   );
--- src/shaders/base-shader/vertex.vert
+++ src/shaders/base-shader/vertex.vert
@@ -4,7 +4,10 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 
 void main() {
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    gl_Position.x += 0.5;
-    gl_Position.y += 0.5;
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
 }
