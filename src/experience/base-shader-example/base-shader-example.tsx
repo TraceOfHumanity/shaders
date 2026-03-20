b7c88f23d0f5d923c9e3f270b8e117675253d13d
TraceOfHumanity <azalexxx02@gmail.com>
2026-03-20 16:05:54 +0200

    feat: enhance base shader example with time-based animation and uniform integration

--- src/experience/base-shader-example/base-shader-example.tsx
+++ src/experience/base-shader-example/base-shader-example.tsx
@@ -1,17 +1,27 @@
 import vertexShader from "../../shaders/base-shader/vertex.vert?raw";
 import fragmentShader from "../../shaders/base-shader/fragment.frag?raw";
-import { DoubleSide } from "three";
+import { DoubleSide, RawShaderMaterial, Uniform } from "three";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
 
 const BaseShaderExample = () => {
+  const materialRef = useRef<RawShaderMaterial>(null);
+
+  useFrame((state) => {
+    const material = materialRef.current;
+    if (material?.uniforms.uTime) {
+      material.uniforms.uTime.value = state.clock.getElapsedTime();
+    }
+  });
   return (
     <>
       <mesh>
         <planeGeometry args={[1, 1, 16, 16]} />
         <rawShaderMaterial
+          ref={materialRef}
           vertexShader={vertexShader}
           fragmentShader={fragmentShader}
-          side={DoubleSide}
-          transparent={true}
+          uniforms={{ uTime: new Uniform(0) }}
         />
       </mesh>
       <mesh position={[-2, 0, 0]}>
