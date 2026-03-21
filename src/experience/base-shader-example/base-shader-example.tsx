d8584c24350b5d398b3a7b781910c2ccb94d949e
TraceOfHumanity <azalexxx02@gmail.com>
2026-03-21 23:45:55 +0200

    feat: implement dynamic vertex updates in base shader example using useEffect

--- src/experience/base-shader-example/base-shader-example.tsx
+++ src/experience/base-shader-example/base-shader-example.tsx
@@ -1,11 +1,13 @@
 import vertexShader from "../../shaders/base-shader/vertex.vert?raw";
 import fragmentShader from "../../shaders/base-shader/fragment.frag?raw";
-import { DoubleSide, RawShaderMaterial, Uniform } from "three";
-import { useRef } from "react";
+import { DoubleSide, Mesh, RawShaderMaterial, Uniform } from "three";
+import { useEffect, useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 
 const BaseShaderExample = () => {
+  const meshRef = useRef<Mesh>(null);
   const materialRef = useRef<RawShaderMaterial>(null);
+  const [vertices, setVertices] = useState<Float32Array>(new Float32Array([]));
 
   useFrame((state) => {
     const material = materialRef.current;
@@ -13,9 +15,20 @@ const BaseShaderExample = () => {
       material.uniforms.uTime.value = state.clock.getElapsedTime();
     }
   });
+
+  useEffect(() => {
+    if (meshRef.current) {
+      const geometry = meshRef.current.geometry;
+      const vertices = geometry.attributes.position.array;
+      for (let i = 0; i < vertices.length; i++) {
+        vertices[i] = Math.random();
+      }
+      setVertices(new Float32Array(vertices));
+    }
+  }, [meshRef]);
   return (
     <>
-      <mesh>
+      <mesh ref={meshRef}>
         <planeGeometry args={[1, 1, 16, 16]} />
         <rawShaderMaterial
           ref={materialRef}
